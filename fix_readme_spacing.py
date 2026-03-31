32a287ef2bfbb69c320b5e588388e7239b6cabf7
Linkohh <103709733+Linkohh@users.noreply.github.com>
2026-03-31 13:26:41 -0400

    fix: reduce separator width in README and update spacing script

--- fix_readme_spacing.py
+++ fix_readme_spacing.py
@@ -3,13 +3,14 @@ import re
 with open('README.md', 'r') as f:
     content = f.read()
 
-# Replace all occurrences of &nbsp;&nbsp; separator &nbsp;&nbsp; with a single &nbsp; and width 14
+# Replace all occurrences of width 14 with width 7
 new_content = re.sub(
-    r'&nbsp;&nbsp;<img src="\./assets/separator\.svg"\s+width="24"\s+alt="_"\s*/>&nbsp;&nbsp;',
-    r'&nbsp;<img src="./assets/separator.svg" width="14" alt="_" />&nbsp;',
+    r'&nbsp;<img src="\./assets/separator\.svg" width="14" alt="_" />&nbsp;',
+    r'&nbsp;<img src="./assets/separator.svg" width="7" alt="_" />&nbsp;',
     content
 )
 
 with open('README.md', 'w') as f:
     f.write(new_content)
+
 
